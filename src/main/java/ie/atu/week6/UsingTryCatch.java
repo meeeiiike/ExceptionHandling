f8e9b025d5273b1abcd58f9d10fbf52ff966407c
G00407095@atu.ie <G00407095@atu.ie>
2024-10-23 14:02:49 +0100

    added handling exception to the code.

--- src/main/java/ie/atu/week6/UsingTryCatch.java
+++ src/main/java/ie/atu/week6/UsingTryCatch.java
@@ -4,17 +4,21 @@ import java.util.Scanner;
 
 public class UsingTryCatch {
     public static void main(String[] args) {
+
         System.out.println("Please enter a number between 0-9: ");
         Scanner in = new Scanner(System.in);
-
         String input = in.nextLine();
-        int number = Integer.parseInt(input);
 
-        if (number >= 0 && number <= 9){
-            System.out.println("Your number is " + number);
-        }
-        else{
-            System.out.println("Not a valid number");
+        try {
+            int number = Integer.parseInt(input);
+
+            if (number >= 0 && number <= 9) {
+                System.out.println("Your number is " + number);
+            } else {
+                System.out.println("Not a valid number");
+            }
+        }catch(NumberFormatException e){
+            System.out.println("Not a valid number! Error: " + e.getMessage());
         }
     }
 }
